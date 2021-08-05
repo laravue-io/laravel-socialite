f87dc2eabe247b77ae370701f6fca229a227b0b4
ayoubbajjou <bajjouayoub@gmail.com>
2021-08-05 13:03:24 +0100

    remove unused class

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\GoogleController;
-use App\Http\Controllers\Auth\LinkedinController;
-use App\Http\Controllers\Auth\FacebookController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
